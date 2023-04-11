8953194ad100730a6d68556f780c913d9ebb1dec
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-04-11 09:57:14 +0530

    Added: Even and odd program

---
+++ NumberEvenOrOdd.java
@@ -0,0 +1,16 @@
+package com.java;
+import java.util.*;
+public class NumberEvenOrOdd {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a numnber to check even or odd");
+        int value=sc.nextInt();
+        if(value%2==04)
+        	System.out.println("Number is Even");
+        else
+        	System.out.println("number is Odd");
+        
+	}
+
+}
