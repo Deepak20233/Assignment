8ec0d3d3c987783c6b1a4c44b2064b3aebd107bb
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-04-11 09:52:50 +0530

    Added:Swap numbers

---
+++ SwapTwoNumber.java
@@ -0,0 +1,19 @@
+package com.java;
+import java.util.*;
+public class SwapTwoNumber {
+
+	public static void main(String[] args) {
+		int temp;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 2 numbers");
+		int num1=sc.nextInt();
+		int num2 =sc.nextInt();
+		
+		temp=num1;
+		num1=num2;
+	    num1=num2;
+	System.out.println("Fist number="+num1);
+	System.out.println("Second number="+num2);
+	}
+
+}
